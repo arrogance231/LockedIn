7c04a1affd6a1695de7c22cbaccd5ec6d0ceb488
arrogance231 <alchemyofjhine@gmail.com>
2025-02-23 00:16:41 +0800

    feat: finished landing page

--- frontend/app/browse/page.tsx
+++ frontend/app/browse/page.tsx
@@ -5,7 +5,7 @@ import { SidebarDemo } from "@/components/ui/sidebar-implementation";
 
 export default async function Home() {
   return (
-    <main className="relative h-screen">
+    <main className="relative h-screen motion-translate-y-in-100 motion-duration-[2s] motion-ease-spring-smooth">
       <NavBar />
       <SidebarDemo />
     </main>
