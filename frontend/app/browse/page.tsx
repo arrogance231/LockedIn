29419a940bf1b129103961cdff4337428a947f2b
arrogance231 <alchemyofjhine@gmail.com>
2025-02-22 08:29:52 +0800

    Template

---
+++ frontend/app/browse/page.tsx
@@ -0,0 +1,9 @@
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <main className="relative h-screen">
+      <NavBar></NavBar>
+    </main>
+  );
+}
